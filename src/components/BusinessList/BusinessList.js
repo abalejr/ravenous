9bb36984f56a5c875a10e4eb29ede94fc9acd20a
A.J. Bale <abalejr@gmail.com>
2019-03-21 15:51:09 -0500

    Fix imports

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './BusinessList.css';
-import Business from '../BusinessList/Business';
+import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render() {
