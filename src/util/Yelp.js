1d731a3591ff964a536f9f62ac662c8d23708ced
A.J. Bale <abalejr@gmail.com>
2019-03-21 23:04:37 -0500

    Add CORS Anywhere URL to request

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const apiKey = 'IIfryINT6bAM7XCyQ940Laqd0Np0M8LJvbDcABtwonwF4ge7odFIJx5jRhmQV4mHohOv-VKmnELNa4jVv78Z1ArEY_VPhkDsbrsaaCkZ0d3uhGjwClK_jN53gFqUXHYx',
       Yelp = {
           search: (term, location, sortBy) => {
-              return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+              return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
           }
 };
