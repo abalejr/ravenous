3b42da5b91f0b53caf651dfcea32f6b1261d4ad4
A.J. Bale <abalejr@gmail.com>
2019-03-21 23:42:36 -0500

    Remove hardcoded business info

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@ import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-},
-      businesses = [
-          business,
-          business,
-          business,
-          business,
-          business,
-          business
-      ];
-
 class App extends Component {
     searchYelp(term, location, sortBy) {
         console.log('Search for: ' + term + ' in ' + location + ' and sort by: ' + sortBy);
